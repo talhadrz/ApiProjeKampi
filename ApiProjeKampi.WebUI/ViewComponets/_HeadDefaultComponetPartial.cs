6369951fc47f1e34c1d0bf54454a5d311b16661b
talhadrz <talhadurmaz36@gmail.com>
2025-03-29 13:02:00 +0300

    Ders 20 Sonu

---
+++ ApiProjeKampi.WebUI/ViewComponets/_HeadDefaultComponetPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponets
+{
+    public class _HeadDefaultComponetPartial :ViewComponent
+    {
+         public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
